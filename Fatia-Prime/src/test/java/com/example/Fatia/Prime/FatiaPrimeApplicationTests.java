85366fd2f6c51137c2452d46d356e4c5fcc7562e
daviramalho-dev <daviramalho101@gmail.com>
2026-09-03 09:44:18 -0300

    feat: adiciona a pasta spring e coloca o index,style e assets dentro de static

---
+++ Fatia-Prime/src/test/java/com/example/Fatia/Prime/FatiaPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fatia.Prime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatiaPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
